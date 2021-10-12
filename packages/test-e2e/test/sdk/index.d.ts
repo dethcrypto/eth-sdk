be9cdba28f04aa6957615f124812559aaeb6eeb4
Kris Kaczor <krzkaczor@gmail.com>
2021-10-12 23:56:46 +0200

    Add test for exported sdk type (#22)

--- packages/test-e2e/test/sdk/index.d.ts
+++ packages/test-e2e/test/sdk/index.d.ts
@@ -1,8 +1,7 @@
-import { Awaited } from 'ts-essentials';
 import { Signer } from 'ethers';
 import * as types from './types';
 export declare function getContract<T>(address: string, abiPath: string, defaultSigner: Signer): T;
-export declare type MainnetSdk = Awaited<ReturnType<typeof getMainnetSdk>>;
+export declare type MainnetSdk = ReturnType<typeof getMainnetSdk>;
 export declare function getMainnetSdk(defaultSigner: Signer): {
     tokens: {
         dai: types.Dai;
