dd0ccf49dbd51bfcbd2647461eaab9fc6ab631de
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-10-12 23:51:03 +0200

    Fix type assert, move ts-essentials to dev dependency

--- packages/test-e2e/test/sdk.test.ts
+++ packages/test-e2e/test/sdk.test.ts
@@ -1,10 +1,10 @@
-import { assert, AssertTrue, IsExact } from 'conditional-type-checks'
+import { assert, IsExact } from 'conditional-type-checks'
 import { expect } from 'earljs'
 import { ethers } from 'ethers'
 import { assert as runtimeAssert } from 'ts-essentials'
 
 import { getMainnetSdk, MainnetSdk } from './sdk'
-import { Dai, Mkr, Uniswap } from './sdk/types'
+import type { Dai, Mkr, Uniswap } from './sdk/types'
 
 describe('sdk', () => {
   it('works', async () => {
@@ -17,8 +17,9 @@ describe('sdk', () => {
 
     expect((await sdk.tokens.dai.totalSupply()).toString()).not.toEqual('0')
     expect((await sdk.tokens.mkr.totalSupply()).toString()).not.toEqual('0')
+
     // assert sdk shape
-    assert<AssertTrue<IsExact<MainnetSdk, typeof sdk>>>(true)
-    assert<AssertTrue<IsExact<MainnetSdk, { tokens: { dai: Dai; mkr: Mkr }; uniswap: Uniswap }>>>(true)
+    assert<IsExact<MainnetSdk, typeof sdk>>(true)
+    assert<IsExact<MainnetSdk, { tokens: { dai: Dai; mkr: Mkr }; uniswap: Uniswap }>>(true)
   })
 })
