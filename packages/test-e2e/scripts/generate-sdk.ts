5057d5b09fe26da3bd37fae0ac3ce0737462ee97
hasparus <hasparus@gmail.com>
2021-11-09 17:12:52 +0100

    Remove test/sdk before running e2e tests

---
+++ packages/test-e2e/scripts/generate-sdk.ts
@@ -0,0 +1,6 @@
+import { execSync } from 'child_process'
+import { rmSync } from 'fs-extra'
+import { resolve } from 'path'
+
+rmSync(resolve(__dirname, '../test/sdk'), { recursive: true, force: true })
+execSync(`ts-node ${resolve(__dirname, '../../eth-sdk/src/cli.ts')}`)
