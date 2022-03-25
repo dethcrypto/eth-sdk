64c8c4c43766e32061b146a78e2e69dd42d45d28
hasparus <hasparus@gmail.com>
2022-03-25 15:47:46 +0100

    Add HTTP 403 Forbidden to status codes to retry on

--- packages/eth-sdk/src/peripherals/fetchJson.ts
+++ packages/eth-sdk/src/peripherals/fetchJson.ts
@@ -1,5 +1,15 @@
-import got from 'got'
+import got, { RequiredRetryOptions } from 'got'
 
 export type FetchJson<T = any> = (url: string) => Promise<T>
 
-export const fetchJson: FetchJson = (url: string) => got(url).json()
+const retryOptions: Partial<RequiredRetryOptions> = {
+  limit: 2,
+  statusCodes: [
+    // defaults
+    408, 413, 429, 500, 502, 503, 504, 521, 522, 524,
+    // added
+    403,
+  ],
+}
+
+export const fetchJson: FetchJson = (url: string) => got(url, { retry: retryOptions }).json()
