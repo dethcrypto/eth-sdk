1c46843e51abf7d739154745623cff22a0357df4
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-06-15 09:40:41 +0200

    Bump typechain artifacts

--- packages/test-e2e/test/with-etherscan/sdk/cjs/types/mainnet/Uniswap.d.ts
+++ packages/test-e2e/test/with-etherscan/sdk/cjs/types/mainnet/Uniswap.d.ts
@@ -1,7 +1,7 @@
 import type { BaseContract, BigNumber, BigNumberish, BytesLike, CallOverrides, ContractTransaction, Overrides, PopulatedTransaction, Signer, utils } from "ethers";
 import type { FunctionFragment, Result, EventFragment } from "@ethersproject/abi";
 import type { Listener, Provider } from "@ethersproject/providers";
-import type { TypedEventFilter, TypedEvent, TypedListener, OnEvent } from "../common";
+import type { TypedEventFilter, TypedEvent, TypedListener, OnEvent, PromiseOrValue } from "../common";
 export interface UniswapInterface extends utils.Interface {
     functions: {
         "createPool(address,address,uint24)": FunctionFragment;
@@ -13,13 +13,21 @@ export interface UniswapInterface extends utils.Interface {
         "setOwner(address)": FunctionFragment;
     };
     getFunction(nameOrSignatureOrTopic: "createPool" | "enableFeeAmount" | "feeAmountTickSpacing" | "getPool" | "owner" | "parameters" | "setOwner"): FunctionFragment;
-    encodeFunctionData(functionFragment: "createPool", values: [string, string, BigNumberish]): string;
-    encodeFunctionData(functionFragment: "enableFeeAmount", values: [BigNumberish, BigNumberish]): string;
-    encodeFunctionData(functionFragment: "feeAmountTickSpacing", values: [BigNumberish]): string;
-    encodeFunctionData(functionFragment: "getPool", values: [string, string, BigNumberish]): string;
+    encodeFunctionData(functionFragment: "createPool", values: [
+        PromiseOrValue<string>,
+        PromiseOrValue<string>,
+        PromiseOrValue<BigNumberish>
+    ]): string;
+    encodeFunctionData(functionFragment: "enableFeeAmount", values: [PromiseOrValue<BigNumberish>, PromiseOrValue<BigNumberish>]): string;
+    encodeFunctionData(functionFragment: "feeAmountTickSpacing", values: [PromiseOrValue<BigNumberish>]): string;
+    encodeFunctionData(functionFragment: "getPool", values: [
+        PromiseOrValue<string>,
+        PromiseOrValue<string>,
+        PromiseOrValue<BigNumberish>
+    ]): string;
     encodeFunctionData(functionFragment: "owner", values?: undefined): string;
     encodeFunctionData(functionFragment: "parameters", values?: undefined): string;
-    encodeFunctionData(functionFragment: "setOwner", values: [string]): string;
+    encodeFunctionData(functionFragment: "setOwner", values: [PromiseOrValue<string>]): string;
     decodeFunctionResult(functionFragment: "createPool", data: BytesLike): Result;
     decodeFunctionResult(functionFragment: "enableFeeAmount", data: BytesLike): Result;
     decodeFunctionResult(functionFragment: "feeAmountTickSpacing", data: BytesLike): Result;
@@ -84,14 +92,14 @@ export interface Uniswap extends BaseContract {
     once: OnEvent<this>;
     removeListener: OnEvent<this>;
     functions: {
-        createPool(tokenA: string, tokenB: string, fee: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        createPool(tokenA: PromiseOrValue<string>, tokenB: PromiseOrValue<string>, fee: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<ContractTransaction>;
-        enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        enableFeeAmount(fee: PromiseOrValue<BigNumberish>, tickSpacing: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<ContractTransaction>;
-        feeAmountTickSpacing(arg0: BigNumberish, overrides?: CallOverrides): Promise<[number]>;
-        getPool(arg0: string, arg1: string, arg2: BigNumberish, overrides?: CallOverrides): Promise<[string]>;
+        feeAmountTickSpacing(arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<[number]>;
+        getPool(arg0: PromiseOrValue<string>, arg1: PromiseOrValue<string>, arg2: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<[string]>;
         owner(overrides?: CallOverrides): Promise<[string]>;
         parameters(overrides?: CallOverrides): Promise<[
             string,
@@ -106,18 +114,18 @@ export interface Uniswap extends BaseContract {
             fee: number;
             tickSpacing: number;
         }>;
-        setOwner(_owner: string, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        setOwner(_owner: PromiseOrValue<string>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<ContractTransaction>;
     };
-    createPool(tokenA: string, tokenB: string, fee: BigNumberish, overrides?: Overrides & {
-        from?: string | Promise<string>;
+    createPool(tokenA: PromiseOrValue<string>, tokenB: PromiseOrValue<string>, fee: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+        from?: PromiseOrValue<string>;
     }): Promise<ContractTransaction>;
-    enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish, overrides?: Overrides & {
-        from?: string | Promise<string>;
+    enableFeeAmount(fee: PromiseOrValue<BigNumberish>, tickSpacing: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+        from?: PromiseOrValue<string>;
     }): Promise<ContractTransaction>;
-    feeAmountTickSpacing(arg0: BigNumberish, overrides?: CallOverrides): Promise<number>;
-    getPool(arg0: string, arg1: string, arg2: BigNumberish, overrides?: CallOverrides): Promise<string>;
+    feeAmountTickSpacing(arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<number>;
+    getPool(arg0: PromiseOrValue<string>, arg1: PromiseOrValue<string>, arg2: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<string>;
     owner(overrides?: CallOverrides): Promise<string>;
     parameters(overrides?: CallOverrides): Promise<[
         string,
@@ -132,14 +140,14 @@ export interface Uniswap extends BaseContract {
         fee: number;
         tickSpacing: number;
     }>;
-    setOwner(_owner: string, overrides?: Overrides & {
-        from?: string | Promise<string>;
+    setOwner(_owner: PromiseOrValue<string>, overrides?: Overrides & {
+        from?: PromiseOrValue<string>;
     }): Promise<ContractTransaction>;
     callStatic: {
-        createPool(tokenA: string, tokenB: string, fee: BigNumberish, overrides?: CallOverrides): Promise<string>;
-        enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish, overrides?: CallOverrides): Promise<void>;
-        feeAmountTickSpacing(arg0: BigNumberish, overrides?: CallOverrides): Promise<number>;
-        getPool(arg0: string, arg1: string, arg2: BigNumberish, overrides?: CallOverrides): Promise<string>;
+        createPool(tokenA: PromiseOrValue<string>, tokenB: PromiseOrValue<string>, fee: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<string>;
+        enableFeeAmount(fee: PromiseOrValue<BigNumberish>, tickSpacing: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<void>;
+        feeAmountTickSpacing(arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<number>;
+        getPool(arg0: PromiseOrValue<string>, arg1: PromiseOrValue<string>, arg2: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<string>;
         owner(overrides?: CallOverrides): Promise<string>;
         parameters(overrides?: CallOverrides): Promise<[
             string,
@@ -154,44 +162,44 @@ export interface Uniswap extends BaseContract {
             fee: number;
             tickSpacing: number;
         }>;
-        setOwner(_owner: string, overrides?: CallOverrides): Promise<void>;
+        setOwner(_owner: PromiseOrValue<string>, overrides?: CallOverrides): Promise<void>;
     };
     filters: {
-        "FeeAmountEnabled(uint24,int24)"(fee?: BigNumberish | null, tickSpacing?: BigNumberish | null): FeeAmountEnabledEventFilter;
-        FeeAmountEnabled(fee?: BigNumberish | null, tickSpacing?: BigNumberish | null): FeeAmountEnabledEventFilter;
-        "OwnerChanged(address,address)"(oldOwner?: string | null, newOwner?: string | null): OwnerChangedEventFilter;
-        OwnerChanged(oldOwner?: string | null, newOwner?: string | null): OwnerChangedEventFilter;
-        "PoolCreated(address,address,uint24,int24,address)"(token0?: string | null, token1?: string | null, fee?: BigNumberish | null, tickSpacing?: null, pool?: null): PoolCreatedEventFilter;
-        PoolCreated(token0?: string | null, token1?: string | null, fee?: BigNumberish | null, tickSpacing?: null, pool?: null): PoolCreatedEventFilter;
+        "FeeAmountEnabled(uint24,int24)"(fee?: PromiseOrValue<BigNumberish> | null, tickSpacing?: PromiseOrValue<BigNumberish> | null): FeeAmountEnabledEventFilter;
+        FeeAmountEnabled(fee?: PromiseOrValue<BigNumberish> | null, tickSpacing?: PromiseOrValue<BigNumberish> | null): FeeAmountEnabledEventFilter;
+        "OwnerChanged(address,address)"(oldOwner?: PromiseOrValue<string> | null, newOwner?: PromiseOrValue<string> | null): OwnerChangedEventFilter;
+        OwnerChanged(oldOwner?: PromiseOrValue<string> | null, newOwner?: PromiseOrValue<string> | null): OwnerChangedEventFilter;
+        "PoolCreated(address,address,uint24,int24,address)"(token0?: PromiseOrValue<string> | null, token1?: PromiseOrValue<string> | null, fee?: PromiseOrValue<BigNumberish> | null, tickSpacing?: null, pool?: null): PoolCreatedEventFilter;
+        PoolCreated(token0?: PromiseOrValue<string> | null, token1?: PromiseOrValue<string> | null, fee?: PromiseOrValue<BigNumberish> | null, tickSpacing?: null, pool?: null): PoolCreatedEventFilter;
     };
     estimateGas: {
-        createPool(tokenA: string, tokenB: string, fee: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        createPool(tokenA: PromiseOrValue<string>, tokenB: PromiseOrValue<string>, fee: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<BigNumber>;
-        enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        enableFeeAmount(fee: PromiseOrValue<BigNumberish>, tickSpacing: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<BigNumber>;
-        feeAmountTickSpacing(arg0: BigNumberish, overrides?: CallOverrides): Promise<BigNumber>;
-        getPool(arg0: string, arg1: string, arg2: BigNumberish, overrides?: CallOverrides): Promise<BigNumber>;
+        feeAmountTickSpacing(arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<BigNumber>;
+        getPool(arg0: PromiseOrValue<string>, arg1: PromiseOrValue<string>, arg2: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<BigNumber>;
         owner(overrides?: CallOverrides): Promise<BigNumber>;
         parameters(overrides?: CallOverrides): Promise<BigNumber>;
-        setOwner(_owner: string, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        setOwner(_owner: PromiseOrValue<string>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<BigNumber>;
     };
     populateTransaction: {
-        createPool(tokenA: string, tokenB: string, fee: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        createPool(tokenA: PromiseOrValue<string>, tokenB: PromiseOrValue<string>, fee: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<PopulatedTransaction>;
-        enableFeeAmount(fee: BigNumberish, tickSpacing: BigNumberish, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        enableFeeAmount(fee: PromiseOrValue<BigNumberish>, tickSpacing: PromiseOrValue<BigNumberish>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<PopulatedTransaction>;
-        feeAmountTickSpacing(arg0: BigNumberish, overrides?: CallOverrides): Promise<PopulatedTransaction>;
-        getPool(arg0: string, arg1: string, arg2: BigNumberish, overrides?: CallOverrides): Promise<PopulatedTransaction>;
+        feeAmountTickSpacing(arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<PopulatedTransaction>;
+        getPool(arg0: PromiseOrValue<string>, arg1: PromiseOrValue<string>, arg2: PromiseOrValue<BigNumberish>, overrides?: CallOverrides): Promise<PopulatedTransaction>;
         owner(overrides?: CallOverrides): Promise<PopulatedTransaction>;
         parameters(overrides?: CallOverrides): Promise<PopulatedTransaction>;
-        setOwner(_owner: string, overrides?: Overrides & {
-            from?: string | Promise<string>;
+        setOwner(_owner: PromiseOrValue<string>, overrides?: Overrides & {
+            from?: PromiseOrValue<string>;
         }): Promise<PopulatedTransaction>;
     };
 }
