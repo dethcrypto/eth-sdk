26749452c71399d4842799af6c12bc13170d8686
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-05-14 10:05:14 +0200

    Improve CI stability

--- packages/test-e2e/test/with-etherscanlikes/generate.ts
+++ packages/test-e2e/test/with-etherscanlikes/generate.ts
@@ -3,4 +3,5 @@ import { resolve } from 'path'
 import { generate, rmrf } from '../e2e-utils'
 
 rmrf(resolve(__dirname, './outDir'))
+rmrf(resolve(__dirname, './eth-sdk/abis'))
 generate({ path: '.', cwd: __dirname })
